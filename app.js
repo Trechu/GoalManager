865cd63db143b21cc31d9147840c6aa0874dbde1
Tomasz <tom.pl@op.pl>
2024-08-19 16:05:40 +0200

    User authentication

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express');
+import express from 'express'
+import passport from 'passport'
+import session from 'express-session'
+import { Strategy as LocalStrategy } from 'passport-local'
+import authUser from './authenticaton/userAuth.js'
 
 const app = express();
 const PORT = 3001;
-
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authUser("Jane", "Doe");
 
 app.get('/', function (request, response) {
     response.render('index.ejs');
@@ -11,8 +25,8 @@ app.get('/', function (request, response) {
 
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+        console.log("Server is running. App is listening on port "+ PORT)
     else 
-        console.log("Error occurred, server can't start", error);
+        console.log("Server was not able to start", error);
     }
 );
---
+++ authenticaton/userAuth.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt';
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.LINK);
+
+const authUser = async (username, password, done) => {
+    const client = new MongoClient(process.env.LINK);
+    try {
+        await client.connect();
+
+        // Change accordingly to db structure
+        const db = client.db('GOALS');
+        const collection = db.collection('user');
+        
+        const query = {name: username};
+        const entry = await collection.findOne(query);
+        console.log(entry);
+        bcrypt.compare(password, entry.password, (err, result) => {
+            if (err) {
+                console.error('Error comparing passwords:', err);
+                return;
+            }
+            if (result) {
+                return done(null, entry.name);
+            } else {
+                return done(null, false);
+            }
+        });
+    } catch(err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+}
+
+export default authUser;
