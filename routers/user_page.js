819af865d73a1a811647090499c07ba732db0f70
Tomasz <tom.pl@op.pl>
2024-08-22 12:44:01 +0200

    User page basics

--- routers/user_page.js
+++ routers/user_page.js
@@ -2,16 +2,24 @@ import express from 'express';
 
 var router = express.Router();
 
-const checkAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
+const checkAuthenticated = (request, response, next) => {
+    if (request.isAuthenticated()) {
         return next() 
     }
-   res.redirect("/")
+   response.redirect("/")
 }
 
 
 router.get('/user', checkAuthenticated, (request, response) => {
-    response.send('Hello ' + request.user.username + " !");
+    response.render("user.ejs");
+})
+
+router.post("/logout", (request, response) => {
+    request.logOut(function(err){
+        if(err) 
+            return next(err)
+    })
+    response.redirect("/");
 })
 
 export default router;
