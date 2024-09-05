a95f9ca0a7622fad5f9f26b56fe690823363d866
Tomasz <tom.pl@op.pl>
2024-09-05 14:57:21 +0200

    UI tweaks

--- scripts/post_management_script.js
+++ scripts/post_management_script.js
@@ -43,6 +43,9 @@ function create_step_element(step){
     var stepBlank1 = document.createElement('div');
     stepBlank1.style.width = '5%';
     stepBlank1.style.margin = 'auto';
+    stepBlank1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+  </svg>`
 
     stepDiv.appendChild(stepBlank1);
 
@@ -82,10 +85,31 @@ function create_step_element(step){
     stepStatus.style.whiteSpace = 'nowrap';
     stepStatus.style.textOverflow = 'ellipsis';
     stepStatus.style.overflow = 'hidden';
+
+    var stepStatusBlock = document.createElement('div');
+    stepStatusBlock.style.width = '85%';
+    stepStatusBlock.style.height = '90%';
+    stepStatusBlock.style.margin = 'auto';
+    stepStatusBlock.style.borderRadius = '10px';
+    
+    switch(step["status"]){
+        case "Completed":
+            stepStatusBlock.style.backgroundColor = 'ForestGreen';
+            break;
+        case "In progress":
+            stepStatusBlock.style.backgroundColor = 'DarkOrange';
+            break;
+        case "Suspended":
+            stepStatusBlock.style.backgroundColor = 'DarkRed';
+            break;
+    }
+
     var stepText3 = document.createElement('span');
     stepText3.style.fontSize = '15px';
     stepText3.innerHTML = step["status"];
-    stepStatus.appendChild(stepText3);
+
+    stepStatusBlock.appendChild(stepText3);
+    stepStatus.appendChild(stepStatusBlock);
     stepDiv.appendChild(stepStatus);
 
     stepDiv.appendChild(create_divider());
@@ -189,6 +213,13 @@ function create_goal_element(goal){
     for(let step of goal.steps){
         goalDiv.appendChild(create_step_element(step));    
     }
+
+    var span = document.createElement('span');
+    span.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg> Add step`;
+    span.style.marginLeft = '3%';
+    goalDiv.appendChild(span);
 
     return goalDiv;
 }
